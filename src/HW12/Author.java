9d6562b5f7b7a12a8af18abfee544cb50a6ce519
Elena <elena161284@mail.ru>
2023-05-28 20:43:20 +0300

    Merge remote-tracking branch 'origin/HW12.3' into HW12.4

--- src/HW12/Author.java
+++ src/HW12/Author.java
@@ -1,20 +1,16 @@
 package HW12;
 
 public class Author {
-    private String name;
-    private String surName;
+    private final String name;
+    private final String surName;
 
-    public Author(String name, String surName) {
+    public Author (String name, String surName) {
         this.name = name;
         this.surName = surName;
     }
-
-    public String getName() {
-        return this.name;
-    }
-    public String getSurName() {
-        return this.surName;
-    }
+    public String getName() {return this.name;}
+    public String getSurName() {return this.surName;}
 }
+
 
 
--- src/M2.java
+++ src/M2.java
@@ -2,21 +2,11 @@ import HW12.Author;
 import HW12.Book;
 public class M2 {
     public static void main(String[] args) {
-        Author lnt =new Author("Лев", "Толстой");
-        System.out.println("lnt.name= " + lnt.getName());
-        System.out.println("lnt.surname= " + lnt.getSurName());
-        Book lnt1 = new Book("Детство", 1853);
-        System.out.println("lnt1.title= " + lnt1.getTitle());
-        System.out.println("lnt1.year= " + lnt1.getYear());
+        Author author =new Author("Лев", "Толстой");
+        Book lnt1 = new Book("Детство", author, 1853);
+        System.out.println(lnt1.getTitle()+" " + author.getName()+ " "
+                               + author.getSurName()+ lnt1.getYear());
         lnt1.setYear(1852);
         System.out.println("lnt1.getYear()= " + lnt1.getYear());
-
-
-        Author fmd2 = new Author("Фёдор", "Достоевский");
-        System.out.println("fmd2.name= " + fmd2.getName());
-        System.out.println("fmd2.surname= " + fmd2.getSurName());
-        Book fmd3 = new Book("Бесы", 1872);
-        System.out.println("fmd3.title= " + fmd3.getTitle());
-        System.out.println("fmd3.year= " + fmd3.getYear());
     }
 }
