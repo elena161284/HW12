e71fed806aa021cf63c3784e34ff9ead125cfdb0
Elena <elena161284@mail.ru>
2023-05-28 12:47:43 +0300

    HW12.7

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,11 @@
 import HW12.Author;
-
 import HW12.Book;
-
 public class Main {
     public static void main(String[] args) {
-        Author fmd = new Author("Фёдор","Достоевский");
+        Author fmd = new Author("Фёдор", "Достоевский");
         System.out.println("fmd.name= " + fmd.getName());
         System.out.println("fmd.surname= " + fmd.getSurName());
-
-        Book fmd1 = new Book("идиот",1869);
+        Book fmd1 = new Book("идиот", 1869);
         System.out.println("fmd1.title= " + fmd1.getTitle());
         System.out.println("fmd1.year= " + fmd1.getYear());
         fmd1.setYear(1868);
